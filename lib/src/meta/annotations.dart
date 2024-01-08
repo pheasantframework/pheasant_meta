f0f961217213f59837e9dcba5ed9a275cba6ffcb
Nike <nikechukwu@gmail.com>
2024-01-08 13:36:49 +0100

    annotations for "pheasant_assets"

--- lib/src/meta/annotations.dart
+++ lib/src/meta/annotations.dart
@@ -1,8 +1,11 @@
 import 'objects.dart';
 
-
+/// Declares objects that are only supported in HTML5 Standard. These should therefore not be used outside a HTML5 interface.
 const html5 = AnnotationObject(info: 'This object is only allowed in HTML5');
 
 /// Declares objects that are not supported in HTML5 Standard. These should therefore not be used in a HTML5 interface.
 /// The build system takes note of this and excludes all instances of the object this annotation is annotated to.
 const nohtml5 = RestrictedAnnotation(info: 'This object is not allowed in HTML5');
+
+/// Declares functions or objects that are not working, or are broken
+const broken = RestrictedAnnotation(info: 'This object or function does not work');
