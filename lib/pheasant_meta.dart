9827fb4b550e0aadd8228f3cd0c3e4395cbdf907
Nike <nikechukwu@gmail.com>
2024-01-11 20:35:02 +0100

    Minor Changes

--- lib/pheasant_meta.dart
+++ lib/pheasant_meta.dart
@@ -1,6 +1,3 @@
-/// Support for doing something awesome.
-///
-/// More dartdocs go here.
 library pheasant_meta;
 
 export 'src/meta/metadata.dart';
@@ -9,5 +6,3 @@ export 'src/exceptions/exceptions.dart';
 export 'package:meta/meta.dart';
 export 'package:meta/dart2js.dart';
 export 'package:meta/meta_meta.dart';
-
-// TODO: Export any libraries intended for clients of this package.
--- test/pme_test.dart
+++ test/pme_test.dart
@@ -4,7 +4,6 @@ import 'package:test/test.dart';
 void main() {
   group('A group of tests', () {
 
-
     setUp(() {
       // Additional setup goes here.
     });
