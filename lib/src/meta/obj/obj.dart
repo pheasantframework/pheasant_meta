cda133f4528a1638f7933c1c0f6d231f47c52108
Nike <nikechukwu@gmail.com>
2024-03-06 13:04:12 -0500

    Added Preamble

--- lib/src/meta/obj/obj.dart
+++ lib/src/meta/obj/obj.dart
@@ -1,3 +1,11 @@
+// Copyright (c) 2024 The Pheasant Group. All Rights Reserved.
+// Please see the AUTHORS files for more information.
+// Intellectual property of third-party.
+// 
+// This file, as well as use of the code in it, is governed by an MIT License
+// that can be found in the LICENSE file.
+// You may not use this file except in compliance with the License.
+  
 import 'package:pheasant_meta/src/meta/basic/objects.dart';
 
 /// Declares objects that are not supported in HTML5 Standard. These should therefore not be used in a HTML5 interface.
